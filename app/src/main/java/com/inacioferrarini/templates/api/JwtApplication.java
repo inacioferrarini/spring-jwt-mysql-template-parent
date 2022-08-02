9e1e099bd87792ae6e533823ebda809bbb8aa77d
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2022-08-02 11:25:11 -0300

    Adds logging configuration

--- app/src/main/java/com/inacioferrarini/templates/api/JwtApplication.java
+++ app/src/main/java/com/inacioferrarini/templates/api/JwtApplication.java
@@ -1,16 +1,26 @@
 package com.inacioferrarini.templates.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JwtApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(
                 JwtApplication.class,
                 args
         );
+
+        logger.trace("Hello from Logback - trace");
+        logger.debug("Hello from Logback - debug");
+        logger.info("Hello from Logback - info");
+        logger.warn("Hello from Logback - warn");
+        logger.error("Hello from Logback - error");
     }
 
 }
