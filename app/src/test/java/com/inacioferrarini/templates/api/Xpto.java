29800ce547d77e1345dd1d4774c47d12b2fd8beb
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2022-08-15 16:14:29 -0300

    Adds sample test

---
+++ app/src/test/java/com/inacioferrarini/templates/api/Xpto.java
@@ -0,0 +1,104 @@
+package com.inacioferrarini.templates.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.inacioferrarini.templates.api.base.models.dtos.StringErrorResponseRecord;
+import com.inacioferrarini.templates.api.security.tests.SecurityTestsHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.sql.Timestamp;
+import java.time.Duration;
+import java.util.Date;
+import java.util.Objects;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class Xpto {
+
+    private static final String API_URL = "/api/security/login";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private SecurityTestsHelper securityTestsHelper;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    // ---------------------------------------------------------------------------------
+    // Setup
+    // ---------------------------------------------------------------------------------
+
+    @Before
+    public void before() {
+        securityTestsHelper.deleteAll();
+    }
+
+    @After
+    public void after() {
+        securityTestsHelper.deleteAll();
+    }
+
+    // ---------------------------------------------------------------------------------
+    // Login: Failure: Wrong Password
+    // ---------------------------------------------------------------------------------
+    @Test
+    public void login_wrongPasswordFailure_mustReturnError() throws Exception {
+        // Given
+        securityTestsHelper.createTestUser();
+
+        final String requestBody = "{\"username\":\"Test User\",\"password\":\"123456\"}";
+
+        MvcResult result = mockMvc.perform(post(API_URL)
+                                .content(requestBody)
+                                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                                  .andExpect(status().isUnauthorized())
+                                  .andExpect(jsonPath("$.timestamp", is(notNullValue())))
+                                  .andExpect(jsonPath("$.status", is(HttpStatus.UNAUTHORIZED.value())))
+                                  .andExpect(jsonPath("$.error", is("Invalid username / password combination.")))
+                .andReturn();
+
+        StringErrorResponseRecord response = objectMapper.readValue(
+                result.getResponse().getContentAsString(),
+                StringErrorResponseRecord.class
+        );
+        assertEquals(0L, daysFromNow(Timestamp.valueOf(response.timestamp())));
+
+        assertEquals(0L, securityTestsHelper.countSecurityTokens());
+    }
+
+    // ---------------------------------------------------------------------------------
+    // Helper Methods
+    // ---------------------------------------------------------------------------------
+
+    private long daysFromNow(final Timestamp timestamp) {
+        Timestamp now = new Timestamp(new Date().getTime());
+        return Duration.between(now.toInstant(), timestamp.toInstant()).toDays();
+    }
+
+}
